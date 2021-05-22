b25bb2779c74925501231951ae389136e804848c
Ferocious Bite <ferocious.bite.eve@gmail.com>
2021-05-22 21:54:16 +0300

    add websocket skipper middleware

---
+++ middlewares/Websocket.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/go-logr/logr"
+
+	"github.com/ferocious-space/durableclient/chains"
+	"github.com/ferocious-space/durableclient/cleanhttp"
+)
+
+func Websocket() chains.Middleware {
+	return func(next http.RoundTripper) http.RoundTripper {
+		return chains.RoundTripFunc(
+			func(request *http.Request) (*http.Response, error) {
+				if request.Header.Get("Upgrade") == "websocket" && request.Header.Get("Connection") == "Upgrade" {
+					logr.FromContextOrDiscard(request.Context()).V(2).Info("middleware.Websocket().RoundTripper()")
+					return cleanhttp.DefaultClient().Transport.RoundTrip(request)
+				}
+				return next.RoundTrip(request)
+			},
+		)
+	}
+}
